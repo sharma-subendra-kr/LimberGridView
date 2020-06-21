c5787c4ea1e2cd1a5ee897403f8288e8593cdbc1
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2020-06-21 22:53:17 +0530

    update

--- src/libs/array/arrayUtils.js
+++ src/libs/array/arrayUtils.js
@@ -1,1 +1,12 @@
-export const shuffle = (arr) => {};
+export const shuffle = (arr) => {
+	let temp,
+		len = arr.length,
+		j;
+	for (let i = 0; i < len; i++) {
+		j = Math.floor(Math.random() * (i + 1));
+		temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	return arr;
+};
