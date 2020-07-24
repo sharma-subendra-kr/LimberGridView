476a0b1f53fbeee4c205b379e0340e168ded3404
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2020-07-24 16:09:18 +0530

    license text update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,8 @@
 /*
 
-This is a release of LimberGridView 0.0.0, brought to you by Subendra Kumar Sharma.
-This software is released under the GNU General Public License version 3, or (at your option) any later version.
+LimberGridView, a powerful JavaScript Libary that gives you movable, resizable(any size) and auto-arranging grids.
 
-LimberGridView, a powerful JavaScript Libary that gives you flexible, movable, resizable(any size) and auto-arranging grids. 
-
-Copyright © 2018-2020, Subendra Kumar Sharma. All Rights reserved. (sharma.subendra.kr@gmail.com, sharma.subendrakr@yahoo.com)
+Copyright © 2018-2020, Subendra Kumar Sharma. All Rights reserved. (jobs.sharma.subendra.kr@gmail.com)
 
 This file is part of LimberGridView.
 
@@ -22,76 +19,81 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with LimberGridView.  If not, see <https://www.gnu.org/licenses/>.
 
+Written by Subendra Kumar Sharma.
+
 */
 
 var fs = require("fs");
 var path = require("path");
 
 var MiniCssExtractPlugin = require("mini-css-extract-plugin");
-var miniCssExtractPlugin = new MiniCssExtractPlugin({ filename: "limberGridView-" + process.env.VERSION  + ".css" });
+var miniCssExtractPlugin = new MiniCssExtractPlugin({
+  filename: "limberGridView-" + process.env.VERSION + ".css",
+});
 var WebpackNotifierPlugin = require("webpack-notifier");
 var webpackNotifierPlugin = new WebpackNotifierPlugin({
-    title: "LimberGridView",
-    alwaysNotify: true,
+  title: "LimberGridView",
+  alwaysNotify: true,
 });
 
 module.exports = {
-    mode: process.env.NODE_ENV === "development" ? "development" : "production",
-    devtool:
-        process.env.NODE_ENV === "development"
-            ? "inline-source-map"
-            : "source-map",
-    entry: __dirname + "/src/index.js",
-    optimization: {
-        minimize: process.env.MINIMIZE === "true" ? true : false
-    },
-    output: {
-        path: process.env.NODE_ENV === "development" ? __dirname + "/public/assets" : __dirname + "/dist",
-        publicPath: "assets",
-        filename: "limberGridView-" + process.env.VERSION  + ".js",
-        library: "LimberGridView",
-        libraryTarget: "umd"
-    },
-    devServer: {
-        inline: true,
-        contentBase: "./public",
-        port: process.env.PORT,
-        host: "0.0.0.0",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: "babel-loader",
+  mode: process.env.NODE_ENV === "development" ? "development" : "production",
+  devtool:
+    process.env.NODE_ENV === "development" ? "inline-source-map" : "source-map",
+  entry: __dirname + "/src/index.js",
+  optimization: {
+    minimize: process.env.MINIMIZE === "true" ? true : false,
+  },
+  output: {
+    path:
+      process.env.NODE_ENV === "development"
+        ? __dirname + "/public/assets"
+        : __dirname + "/dist",
+    publicPath: "assets",
+    filename: "limberGridView-" + process.env.VERSION + ".js",
+    library: "LimberGridView",
+    libraryTarget: "umd",
+  },
+  devServer: {
+    inline: true,
+    contentBase: "./public",
+    port: process.env.PORT,
+    host: "0.0.0.0",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        loader: "json-loader",
+      },
+      {
+        test: /\.(sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === "development",
+              reloadAll: true,
             },
-            {
-                test: /\.json$/,
-                exclude: /node_modules/,
-                loader: "json-loader",
-            },
-            {
-                test: /\.(sc|c)ss$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            hmr: process.env.NODE_ENV === "development",
-                            reloadAll: true,
-                        },
-                    },
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader",
-                ],
-            },
+          },
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
         ],
+      },
+    ],
+  },
+  plugins: [miniCssExtractPlugin, webpackNotifierPlugin],
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src/"),
     },
-    plugins: [miniCssExtractPlugin, webpackNotifierPlugin],
-    resolve: {
-        alias: {
-            src: path.resolve(__dirname, "src/"),
-        },
-    },
+  },
 };
