eba3884f45db9a15142e29532086b09f69da3f26
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2020-07-26 03:08:33 +0530

    inserted tabs instead of spaces

--- webpack.config.js
+++ webpack.config.js
@@ -30,92 +30,92 @@ var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var miniCssExtractPlugin = new MiniCssExtractPlugin({ filename: "index.css" });
 var WebpackNotifierPlugin = require("webpack-notifier");
 var webpackNotifierPlugin = new WebpackNotifierPlugin({
-  title: "LimberGridView",
-  alwaysNotify: true,
+	title: "LimberGridView",
+	alwaysNotify: true,
 });
 var Webpack = require("webpack");
 var webpackBannerPlugin = new Webpack.BannerPlugin({
-  banner: fs.readFileSync("./license_header", "utf8"),
-  raw: true,
+	banner: fs.readFileSync("./license_header", "utf8"),
+	raw: true,
 });
 
 module.exports = {
-  mode: process.env.NODE_ENV === "development" ? "development" : "production",
-  devtool:
-    process.env.NODE_ENV === "development" ? "inline-source-map" : "none",
-  entry: __dirname + "/src/index.js",
-  optimization: {
-    minimize: process.env.MINIMIZE === "true" ? true : false,
-  },
-  output: {
-    path:
-      process.env.NODE_ENV === "development"
-        ? __dirname + "/public/assets"
-        : __dirname + "/dist/umd/",
-    publicPath: "assets",
-    filename:
-      process.env.NODE_ENV === "development"
-        ? "index.js"
-        : process.env.MINIMIZE === "true"
-        ? "limber.grid.view.production.min.js"
-        : "limber.grid.view.development.js",
-    library: "LimberGridView",
-    libraryTarget: "umd",
-  },
-  devServer: {
-    inline: true,
-    contentBase: "./public",
-    port: process.env.PORT,
-    host: "0.0.0.0",
-  },
-  externals: {
-    IntervalTreeJS: {
-      commonjs: "IntervalTreeJS",
-      commonjs2: "IntervalTreeJS",
-      amd: "IntervalTreeJS",
-      root: "IntervalTreeJS",
-    },
-    ClosestBST: {
-      commonjs: "ClosestBST",
-      commonjs2: "ClosestBST",
-      amd: "ClosestBST",
-      root: "ClosestBST",
-    },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.json$/,
-        exclude: /node_modules/,
-        loader: "json-loader",
-      },
-      {
-        test: /\.(sc|c)ss$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === "development",
-              reloadAll: true,
-            },
-          },
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-    ],
-  },
-  plugins: [miniCssExtractPlugin, webpackNotifierPlugin, webpackBannerPlugin],
-  resolve: {
-    alias: {
-      src: path.resolve(__dirname, "src/"),
-    },
-  },
+	mode: process.env.NODE_ENV === "development" ? "development" : "production",
+	devtool:
+		process.env.NODE_ENV === "development" ? "inline-source-map" : "none",
+	entry: __dirname + "/src/index.js",
+	optimization: {
+		minimize: process.env.MINIMIZE === "true" ? true : false,
+	},
+	output: {
+		path:
+			process.env.NODE_ENV === "development"
+				? __dirname + "/public/assets"
+				: __dirname + "/dist/umd/",
+		publicPath: "assets",
+		filename:
+			process.env.NODE_ENV === "development"
+				? "index.js"
+				: process.env.MINIMIZE === "true"
+				? "limber.grid.view.production.min.js"
+				: "limber.grid.view.development.js",
+		library: "LimberGridView",
+		libraryTarget: "umd",
+	},
+	devServer: {
+		inline: true,
+		contentBase: "./public",
+		port: process.env.PORT,
+		host: "0.0.0.0",
+	},
+	externals: {
+		IntervalTreeJS: {
+			commonjs: "IntervalTreeJS",
+			commonjs2: "IntervalTreeJS",
+			amd: "IntervalTreeJS",
+			root: "IntervalTreeJS",
+		},
+		ClosestBST: {
+			commonjs: "ClosestBST",
+			commonjs2: "ClosestBST",
+			amd: "ClosestBST",
+			root: "ClosestBST",
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.json$/,
+				exclude: /node_modules/,
+				loader: "json-loader",
+			},
+			{
+				test: /\.(sc|c)ss$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							hmr: process.env.NODE_ENV === "development",
+							reloadAll: true,
+						},
+					},
+					"css-loader",
+					"postcss-loader",
+					"sass-loader",
+				],
+			},
+		],
+	},
+	plugins: [miniCssExtractPlugin, webpackNotifierPlugin, webpackBannerPlugin],
+	resolve: {
+		alias: {
+			src: path.resolve(__dirname, "src/"),
+		},
+	},
 };
