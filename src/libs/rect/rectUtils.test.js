2fa0d8a7dab1ed74ccdc8237cf47c3bfc26b23d2
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2020-11-09 12:21:55 +0530

    tests

--- src/libs/rect/rectUtils.test.js
+++ src/libs/rect/rectUtils.test.js
@@ -1,6 +1,7 @@
-import { subtractRect } from "./rectUtils";
+import { subtractRect, mergeRects } from "./rectUtils";
 
 test("subtractRect", () => {
+	// incomplete tests, should have written tests while testing this thing
 	let rectA = { x: 2, y: 2, width: 4, height: 4 };
 	let rectB = { x: 4, y: 3, width: 4, height: 2 };
 	let res = [
@@ -21,4 +22,19 @@ test("subtractRect", () => {
 		{ x: 2, y: 4, width: 2, height: 2 },
 	];
 	expect(subtractRect(rectA, rectB)).toStrictEqual(res);
+});
+
+test("mergeRects", () => {
+	// incomplete tests, should have written tests while testing this thing
+	let rectA = { x: 2, y: 2, width: 4, height: 4 };
+	let rectB = { x: 4, y: 3, width: 4, height: 2 };
+	let res = [{ x: 2, y: 3, width: 6, height: 2 }];
+
+	expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+	rectA = { x: 2, y: 2, width: 4, height: 4 };
+	rectB = { x: 4, y: 4, width: 4, height: 2 };
+	res = [{ x: 2, y: 4, width: 6, height: 2 }];
+
+	expect(mergeRects(rectA, rectB)).toStrictEqual(res);
 });
