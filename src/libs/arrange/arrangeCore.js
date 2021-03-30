be50e82b1d1821baabe9435cf406a586208284b6
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2021-03-30 21:26:58 +0530

    update

--- src/libs/arrange/arrangeCore.js
+++ src/libs/arrange/arrangeCore.js
@@ -292,105 +292,105 @@ export const sweepLineForFreeSpace = (
 	return { rt };
 };
 
-export const mergeFreeRectsCore = (context, stack, rt, idCount, direction) => {
-	const resStack = getStack(context, "resStack");
+// export const mergeFreeRectsCore = (context, stack, rt, idCount, direction) => {
+// 	const resStack = getStack(context, "resStack");
 
-	const dummy = { x1: 0, x2: 0, y1: 0, y2: 0 };
-	const findRect = { x1: 0, x2: 0, y1: 0, y2: 0 };
-	let topFullMerged;
-	while (!stack.isEmpty()) {
-		const top = stack.pop();
-		topFullMerged = undefined;
+// 	const dummy = { x1: 0, x2: 0, y1: 0, y2: 0 };
+// 	const findRect = { x1: 0, x2: 0, y1: 0, y2: 0 };
+// 	let topFullMerged;
+// 	while (!stack.isEmpty()) {
+// 		const top = stack.pop();
+// 		topFullMerged = undefined;
 
-		findRect.x1 = top.rect.x1;
-		findRect.x2 = top.rect.x2;
-		findRect.y1 = top.rect.y1;
-		findRect.y2 = top.rect.y2;
+// 		findRect.x1 = top.rect.x1;
+// 		findRect.x2 = top.rect.x2;
+// 		findRect.y1 = top.rect.y1;
+// 		findRect.y2 = top.rect.y2;
 
-		if (direction === 1) {
-			// x
-			// findRect.y2 += 1;
-		} else {
-			// y
-			// findRect.x2 += 1;
-		}
+// 		if (direction === 1) {
+// 			// x
+// 			// findRect.y2 += 1;
+// 		} else {
+// 			// y
+// 			// findRect.x2 += 1;
+// 		}
 
-		const results = rt.find(findRect, false, true, undefined, true);
+// 		const results = rt.find(findRect, false, true, undefined, true);
 
-		const len = results?.length || 0;
-		if (len > 0) {
-			for (let i = 0; i < len; i++) {
-				const res = results[i];
+// 		const len = results?.length || 0;
+// 		if (len > 0) {
+// 			for (let i = 0; i < len; i++) {
+// 				const res = results[i];
 
-				const mergedRects = mergeRects(
-					getRectObjectFromRTreeRect(res.rect),
-					getRectObjectFromRTreeRect(top.rect)
-				);
-				if (mergedRects.length === 1) {
-					const mergedRect = mergedRects[0];
-					const mergedRectRTreeFormat = getRTreeRectFromRectObject(mergedRect);
+// 				const mergedRects = mergeRects(
+// 					getRectObjectFromRTreeRect(res.rect),
+// 					getRectObjectFromRTreeRect(top.rect)
+// 				);
+// 				if (mergedRects.length === 1) {
+// 					const mergedRect = mergedRects[0];
+// 					const mergedRectRTreeFormat = getRTreeRectFromRectObject(mergedRect);
 
-					const mergedObject = {
-						rect: mergedRectRTreeFormat,
-						data: {
-							id: idCount.idCount++,
-						},
-					};
+// 					const mergedObject = {
+// 						rect: mergedRectRTreeFormat,
+// 						data: {
+// 							id: idCount.idCount++,
+// 						},
+// 					};
 
-					if (isRectInside(mergedRect, getRectObjectFromRTreeRect(res.rect))) {
-						rt.remove(res.rect);
-					}
+// 					if (isRectInside(mergedRect, getRectObjectFromRTreeRect(res.rect))) {
+// 						rt.remove(res.rect);
+// 					}
 
-					if (
-						isRectInside(mergedRect, getRectObjectFromRTreeRect(top.rect)) &&
-						isRectLarger(mergedRectRTreeFormat, topFullMerged?.rect || dummy)
-					) {
-						topFullMerged = mergedObject;
-					}
+// 					if (
+// 						isRectInside(mergedRect, getRectObjectFromRTreeRect(top.rect)) &&
+// 						isRectLarger(mergedRectRTreeFormat, topFullMerged?.rect || dummy)
+// 					) {
+// 						topFullMerged = mergedObject;
+// 					}
 
-					// rt.insert(mergedObject);
-					resStack.push(mergedObject);
-				}
-			}
-			if (!topFullMerged) {
-				// put  top in the tree
-				rt.insert({ rect: top.rect, data: { id: idCount.idCount++ } });
+// 					// rt.insert(mergedObject);
+// 					resStack.push(mergedObject);
+// 				}
+// 			}
+// 			if (!topFullMerged) {
+// 				// put  top in the tree
+// 				rt.insert({ rect: top.rect, data: { id: idCount.idCount++ } });
 
-				while (!resStack.isEmpty()) {
-					const stackTop = resStack.pop();
-					rt.insert(stackTop);
-				}
-			} else {
-				rt.insert(topFullMerged);
-			}
-		} else {
-			rt.insert({ rect: top.rect, data: { id: idCount.idCount++ } });
-		}
-	}
-};
+// 				while (!resStack.isEmpty()) {
+// 					const stackTop = resStack.pop();
+// 					rt.insert(stackTop);
+// 				}
+// 			} else {
+// 				rt.insert(topFullMerged);
+// 			}
+// 		} else {
+// 			rt.insert({ rect: top.rect, data: { id: idCount.idCount++ } });
+// 		}
+// 	}
+// };
 
-export const filterMergedFreeRects = (rt) => {
-	// const removeMap = {};
-	const arr = rt.getData();
-	const len = arr.length;
-	for (let i = 0; i < len; i++) {
-		const obj = arr[i];
-		const result = rt.find(
-			obj.rect,
-			undefined,
-			undefined,
-			shouldFilterRect,
-			false
-		);
+// export const filterMergedFreeRects = (rt) => {
+// 	// const removeMap = {};
+// 	const arr = rt.getData();
+// 	const len = arr.length;
+// 	for (let i = 0; i < len; i++) {
+// 		const obj = arr[i];
+// 		const result = rt.find(
+// 			obj.rect,
+// 			undefined,
+// 			undefined,
+// 			shouldFilterRect,
+// 			false
+// 		);
 
-		if (result) {
-			// removeMap[i] = true;
-			rt.remove(obj.rect);
-		}
-	}
+// 		if (result) {
+// 			// removeMap[i] = true;
+// 			rt.remove(obj.rect);
+// 		}
+// 	}
 
-	// return arr.filter((o, index) => removeMap[index]);
-};
+// 	// return arr.filter((o, index) => removeMap[index]);
+// };
 
 // export const mergeFreeRects = async (
 // 	context,
@@ -426,6 +426,91 @@ export const filterMergedFreeRects = (rt) => {
 // 	return { mergedRectsRt: rt };
 // };
 
+export const mergeFreeRectsCore = async (context, arr, mergeCount, idCount) => {
+	const stack = getStack(context, "stack");
+
+	stack.empty();
+	stack.push(arr[0]);
+	const len = arr.length;
+	for (let i = 1; i < len; i++) {
+		printMergedRect(context);
+		printStackTopRect(context);
+		printAdjRect(context);
+
+		const item = arr[i];
+		const top = stack.peak();
+		printStackTopRect(context, top);
+		printAdjRect(context, item);
+
+		const itemR = getRectObjectFromRTreeRect(item.rect);
+		const topR = getRectObjectFromRTreeRect(top.rect);
+
+		const mergedRects = mergeRects(itemR, topR);
+		if (doRectsOverlapOrTouch(itemR, topR) && mergedRects.length === 1) {
+			mergeCount.mergeCount++;
+			const mergedRect = mergedRects[0];
+			if (isRectInside(mergedRect, topR)) {
+				stack.pop();
+			}
+
+			if (!isRectInside(mergedRect, itemR)) {
+				stack.push(item);
+			}
+
+			stack.push({
+				rect: getRTreeRectFromRectObject(mergedRect),
+				data: {
+					id: idCount.idCount++,
+				},
+			});
+			printMergedRect(context, stack.peak());
+		} else {
+			let ptr = stack.ptr - 1;
+			let ptrItem = stack.stack[ptr];
+			const belowTopItem = ptrItem;
+			let isInside = false;
+			let mergedRectObj;
+			printStackTopRect(context, ptrItem);
+			while (ptr >= 0 && belowTopItem.rect.x1 === ptrItem.rect.x1) {
+				const ptrR = getRectObjectFromRTreeRect(ptrItem.rect);
+
+				const mergedRects = mergeRects(itemR, ptrR);
+				if (doRectsOverlapOrTouch(itemR, ptrR) && mergedRects.length === 1) {
+					mergeCount.mergeCount++;
+					const mergedRect = mergedRects[0];
+					if (isRectInside(mergedRect, ptrR)) {
+						ptrItem.rect.x1 = 0;
+						ptrItem.rect.y1 = 0;
+						ptrItem.rect.x2 = 0;
+						ptrItem.rect.y2 = 0;
+					}
+
+					if (isRectInside(mergedRect, itemR)) {
+						isInside = true;
+					}
+
+					mergedRectObj = {
+						rect: getRTreeRectFromRectObject(mergedRect),
+						data: {
+							id: idCount.idCount++,
+						},
+					};
+					break;
+				}
+				ptr--;
+				ptrItem = stack.stack[ptr];
+			}
+			if (!isInside) {
+				stack.push(item);
+			}
+			if (mergedRectObj) {
+				stack.push(mergedRectObj);
+				printMergedRect(context, stack.peak());
+			}
+		}
+	}
+};
+
 export const mergeFreeRects = async (
 	context,
 	freeRects,
@@ -446,94 +531,16 @@ export const mergeFreeRects = async (
 	}
 
 	printUnmergedFreeRects(context, freeRects);
+	// throw "";
 	debugger;
-	let mergeCount;
+	const mergeCount = { mergeCount: 0 };
 	let arr;
 	let len;
 	do {
-		mergeCount = 0;
+		mergeCount.mergeCount = 0;
 
 		arr = freeRects.sort(rectSortX);
-		stack.empty();
-		stack.push(arr[0]);
-		len = arr.length;
-		for (let i = 1; i < len; i++) {
-			printMergedRect(context);
-			printStackTopRect(context);
-			printAdjRect(context);
-
-			const item = arr[i];
-			const top = stack.peak();
-			printStackTopRect(context, top);
-			printAdjRect(context, item);
-
-			const itemR = getRectObjectFromRTreeRect(item.rect);
-			const topR = getRectObjectFromRTreeRect(top.rect);
-
-			const mergedRects = mergeRects(itemR, topR);
-			if (doRectsOverlapOrTouch(itemR, topR) && mergedRects.length === 1) {
-				mergeCount++;
-				const mergedRect = mergedRects[0];
-				if (isRectInside(mergedRect, topR)) {
-					stack.pop();
-				}
-
-				if (!isRectInside(mergedRect, itemR)) {
-					stack.push(item);
-				}
-
-				stack.push({
-					rect: getRTreeRectFromRectObject(mergedRect),
-					data: {
-						id: idCount.idCount++,
-					},
-				});
-				printMergedRect(context, stack.peak());
-			} else {
-				let ptr = stack.ptr - 1;
-				let ptrItem = stack.stack[ptr];
-				const belowTopItem = ptrItem;
-				let isInside = false;
-				let mergedRectObj;
-				printStackTopRect(context, ptrItem);
-				while (ptr >= 0 && belowTopItem.rect.x1 === ptrItem.rect.x1) {
-					const ptrR = getRectObjectFromRTreeRect(ptrItem.rect);
-
-					const mergedRects = mergeRects(itemR, ptrR);
-					if (doRectsOverlapOrTouch(itemR, ptrR) && mergedRects.length === 1) {
-						mergeCount++;
-						const mergedRect = mergedRects[0];
-						if (isRectInside(mergedRect, ptrR)) {
-							ptrItem.rect.x1 = 0;
-							ptrItem.rect.y1 = 0;
-							ptrItem.rect.x2 = 0;
-							ptrItem.rect.y2 = 0;
-						}
-
-						if (isRectInside(mergedRect, itemR)) {
-							isInside = true;
-						}
-
-						mergedRectObj = {
-							rect: getRTreeRectFromRectObject(mergedRect),
-							data: {
-								id: idCount.idCount++,
-							},
-						};
-						break;
-					}
-					ptr--;
-					ptrItem = stack.stack[ptr];
-				}
-				if (!isInside) {
-					stack.push(item);
-				}
-				if (mergedRectObj) {
-					stack.push(mergedRectObj);
-					printMergedRect(context, stack.peak());
-				}
-			}
-		}
+		mergeFreeRectsCore(context, arr, mergeCount, idCount);
 	} while (false);
 	// } while (mergeCount > 0);
 
