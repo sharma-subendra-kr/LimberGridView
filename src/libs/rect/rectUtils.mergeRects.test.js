9b6fe77c6285e643fa0d444b37338d54b7dd7aa9
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2021-04-06 12:58:05 +0530

    mergeRects: test cases

--- src/libs/rect/rectUtils.mergeRects.test.js
+++ src/libs/rect/rectUtils.mergeRects.test.js
@@ -209,3 +209,199 @@ describe("mergeRects", () => {
 		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
 	});
 });
+
+describe("mergeRects reverse", () => {
+	test("right", () => {
+		let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		let rectB = { x1: 0, y1: 5, x2: 2, y2: 6 };
+		let res = [{ x1: 0, y1: 5, x2: 6, y2: 6 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 2, x2: 2, y2: 6 };
+		res = [{ x1: 0, y1: 2, x2: 6, y2: 6 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 1, x2: 2, y2: 6 };
+		res = [{ x1: 0, y1: 2, x2: 6, y2: 6 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 3, x2: 2, y2: 4 };
+		res = [{ x1: 0, y1: 3, x2: 6, y2: 4 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 2, x2: 2, y2: 4 };
+		res = [{ x1: 0, y1: 2, x2: 6, y2: 4 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 1, x2: 2, y2: 4 };
+		res = [{ x1: 0, y1: 2, x2: 6, y2: 4 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+
+	test("right above", () => {
+		let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		let rectB = { x1: 0, y1: 5, x2: 2, y2: 7 };
+		let res = [{ x1: 0, y1: 5, x2: 6, y2: 6 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 2, x2: 2, y2: 7 };
+		res = [{ x1: 0, y1: 2, x2: 6, y2: 6 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 1, x2: 2, y2: 7 };
+		res = [{ x1: 0, y1: 2, x2: 6, y2: 6 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+
+	test("top", () => {
+		let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		let rectB = { x1: 2, y1: 6, x2: 3, y2: 7 };
+		let res = [{ x1: 2, y1: 2, x2: 3, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 2, y1: 6, x2: 6, y2: 7 };
+		res = [{ x1: 2, y1: 2, x2: 6, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 2, y1: 6, x2: 7, y2: 7 };
+		res = [{ x1: 2, y1: 2, x2: 6, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 3, y1: 6, x2: 5, y2: 7 };
+		res = [{ x1: 3, y1: 2, x2: 5, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 3, y1: 6, x2: 6, y2: 7 };
+		res = [{ x1: 3, y1: 2, x2: 6, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 3, y1: 6, x2: 7, y2: 7 };
+		res = [{ x1: 3, y1: 2, x2: 6, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+
+	test("top left", () => {
+		let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		let rectB = { x1: 0, y1: 6, x2: 3, y2: 7 };
+		let res = [{ x1: 2, y1: 2, x2: 3, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 6, x2: 6, y2: 7 };
+		res = [{ x1: 2, y1: 2, x2: 6, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+
+		rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		rectB = { x1: 0, y1: 6, x2: 7, y2: 7 };
+		res = [{ x1: 2, y1: 2, x2: 6, y2: 7 }];
+		expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+
+	test("right  overlapping", () => {
+		// let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// let rectB = { x1: 5, y1: 5, x2: 8, y2: 6 };
+		// let res = [{ x1: 2, y1: 5, x2: 8, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 5, y1: 2, x2: 8, y2: 6 };
+		// res = [{ x1: 2, y1: 2, x2: 8, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 5, y1: 1, x2: 8, y2: 6 };
+		// res = [{ x1: 2, y1: 2, x2: 8, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 5, y1: 3, x2: 8, y2: 4 };
+		// res = [{ x1: 2, y1: 3, x2: 8, y2: 4 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 5, y1: 2, x2: 8, y2: 4 };
+		// res = [{ x1: 2, y1: 2, x2: 8, y2: 4 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 5, y1: 1, x2: 8, y2: 4 };
+		// res = [
+		// 	{ x1: 5, y1: 1, x2: 6, y2: 6 },
+		// 	{ x1: 2, y1: 2, x2: 8, y2: 4 },
+		// ];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+
+	test("right above  overlapping", () => {
+		// let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// let rectB = { x1: 5, y1: 5, x2: 8, y2: 7 };
+		// let res = [
+		// 	{ x1: 5, y1: 2, x2: 6, y2: 7 },
+		// 	{ x1: 2, y1: 5, x2: 8, y2: 6 },
+		// ];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 5, y1: 2, x2: 8, y2: 7 };
+		// res = [{ x1: 2, y1: 2, x2: 8, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 5, y1: 1, x2: 8, y2: 7 };
+		// res = [{ x1: 2, y1: 2, x2: 8, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+
+	test("top  overlapping", () => {
+		// let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// let rectB = { x1: 2, y1: 1, x2: 3, y2: 3 };
+		// let res = [{ x1: 2, y1: 1, x2: 3, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 2, y1: 1, x2: 6, y2: 3 };
+		// res = [{ x1: 2, y1: 1, x2: 6, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 2, y1: 1, x2: 8, y2: 3 };
+		// res = [{ x1: 2, y1: 1, x2: 6, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 3, y1: 1, x2: 5, y2: 3 };
+		// res = [{ x1: 3, y1: 1, x2: 5, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 3, y1: 1, x2: 6, y2: 3 };
+		// res = [{ x1: 3, y1: 1, x2: 6, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 3, y1: 1, x2: 7, y2: 3 };
+		// res = [
+		// 	{ x1: 3, y1: 1, x2: 6, y2: 6 },
+		// 	{ x1: 2, y1: 2, x2: 7, y2: 3 },
+		// ];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+
+	test("top left overlapping", () => {
+		// let rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// let rectB = { x1: 1, y1: 1, x2: 3, y2: 3 };
+		// let res = [
+		// 	{ x1: 1, y1: 2, x2: 6, y2: 3 },
+		// 	{ x1: 2, y1: 1, x2: 3, y2: 6 },
+		// ];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 1, y1: 1, x2: 6, y2: 3 };
+		// res = [{ x1: 2, y1: 1, x2: 6, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+		// rectA = { x1: 2, y1: 2, x2: 6, y2: 6 };
+		// rectB = { x1: 1, y1: 1, x2: 7, y2: 3 };
+		// res = [{ x1: 2, y1: 1, x2: 6, y2: 6 }];
+		// expect(mergeRects(rectA, rectB)).toStrictEqual(res);
+	});
+});
