6db32869070dd3695783414708e5021867e64575
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2021-04-13 21:20:05 +0530

    Issue #76: done

--- src/libs/eventHandlerLib/miscellaneous.js
+++ src/libs/eventHandlerLib/miscellaneous.js
@@ -24,7 +24,10 @@ Written by Subendra Kumar Sharma.
 */
 
 import getPublicConstants from "../../store/constants/publicConstants";
-import { getCallbacks } from "../../store/variables/essentials";
+import {
+	getCallbacks,
+	setLimberGridViewBoundingClientRect,
+} from "../../store/variables/essentials";
 import { getBindedFunctions } from "../../store/variables/bindedFunctions";
 import { init } from "../../initializers/initializers";
 import { render } from "../renderers/renderers";
@@ -41,6 +44,12 @@ export const onItemClick = function (event) {
 };
 
 export const instantiateResizeObserver = function () {
+	setLimberGridViewBoundingClientRect(
+		this,
+		getAllBoundingClientRectKeys(
+			get$limberGridView(this).getBoundingClientRect()
+		)
+	);
 	this.store.observer.resizeObserver.resizeObserver = new ResizeObserver(
 		getBindedFunctions(this).resizeObserverCallback
 	);
@@ -57,6 +66,25 @@ export const resizeObserverCallback = function () {
 			await init(this, true, false);
 			render(this);
 			setIsResizeObserving(this, false);
+			setLimberGridViewBoundingClientRect(
+				this,
+				getAllBoundingClientRectKeys(
+					get$limberGridView(this).getBoundingClientRect()
+				)
+			);
 		}, publicConstants.WINDOW_RESIZE_WAIT_TIME);
 	}
+};
+
+export const getAllBoundingClientRectKeys = function (rect) {
+	return {
+		x: rect.x,
+		y: rect.y,
+		width: rect.width,
+		height: rect.height,
+		top: rect.top,
+		right: rect.right,
+		bottom: rect.bottom,
+		left: rect.left,
+	};
 };
