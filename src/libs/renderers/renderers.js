cec2cb9a8fc575b51e5528ef788494fa8c42e15f
Subendra Kumar Sharma <sharma.subendra.kr@gmail.com>
2021-05-24 19:37:35 +0530

    bugfix: removing the wrong index from renderedItems array

--- src/libs/renderers/renderers.js
+++ src/libs/renderers/renderers.js
@@ -524,8 +524,8 @@ export const removeItem = function (context, index) {
 	}
 
 	const renderedItems = getRenderedItems(context);
-	if (renderedItems.find((o) => o === index)) {
-		renderedItems.length = renderedItems.length - 1;
+	if (renderedItems.find((o) => o === index) >= 0) {
+		renderedItems.splice(renderedItems.indexOf(index), 1);
 	}
 
 	initializeEvents.call(context);
